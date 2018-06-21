d84a46ae2aa505ecb830a9e87e5f98e5f26cd33a
Andy Lo <squeakyspacebar@gmail.com>
2018-06-21 10:20:45 -0500

    Added use case endpoint for API.

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -12,4 +12,38 @@ router.get('/', (req, res) => {
   }
 });
 
+router.get('/use-cases/', (req, res) => {
+  try {
+    models.use_case.findAll({
+      include: [
+        {
+          model: models.profile,
+          include: [
+            {
+              model: models.location,
+              include: [
+                {
+                  model: models.country,
+                  include: [
+                    models.region,
+                  ],
+                },
+              ],
+            },
+            models.status,
+            models.industry,
+          ],
+        }
+      ],
+      order: [
+        [models.profile, 'org_name', 'ASC'],
+      ],
+    }).then((values) => {
+      res.send(values);
+    });
+  } catch (err) {
+    handleError(err);
+  }
+});
+
 module.exports = router;
